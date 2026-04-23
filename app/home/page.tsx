1b07c5c2d16999f299e9bf64fa1ee28ab0c259f9
hn-e <pigeon.himanshu@gmail.com>
2026-04-23 18:48:27 +0530

    Link fix

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import LadyModel from "@/components/lady-model";
 
 export default function HomePage() {
@@ -5,12 +6,12 @@ export default function HomePage() {
     <div className="relative min-h-screen">
       <LadyModel />
       <div className="absolute bottom-10 left-1/2 -translate-x-1/2">
-        <a
+        <Link
           href="/"
           className="rounded-md bg-primary px-6 py-3 text-sm font-medium text-primary-foreground shadow-xs hover:bg-primary/90"
         >
           Get Started
-        </a>
+        </Link>
       </div>
     </div>
   );
